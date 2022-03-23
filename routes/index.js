52637712f636c864762b677636f3b282daa75073
seoyeon <seoyeon@DESKTOP-QNRDN1R.localdomain>
2022-03-23 22:35:16 +0900

    ADD: routes폴더 안 index.js생성 후 코드 작성

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const userRoute = require('./userRoute')
+const productRoute = require('./productRoute')
+
+router.use('/user', userRoute)
+router.use('./products', productRoute)
+
+module.exports = router
